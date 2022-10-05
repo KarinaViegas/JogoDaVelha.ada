1c5b8f13ae6001cf2e8f1e32c4ab3461d29e00af
Karina Viegas <karinaviegas19@gmail.com>
2022-10-05 00:53:33 -0300

    Adicionando validação de erros

--- src/src/Jogadas.java
+++ src/src/Jogadas.java
@@ -7,18 +7,24 @@ public class Jogadas {
         System.out.printf("%n%s, escolha em qual posicão você deseja jogar:%n", vez);
         posicaoJogada = scanner.nextInt();
         posicaoJogada -= 1;//para funcionar com o array que começa com zero
-        if (tabuleiro[posicaoJogada].equals("X") || tabuleiro[posicaoJogada].equals("O")) {
-            System.out.println("Posição inválida");
+
+        try{
+            if (tabuleiro[posicaoJogada].equals("X") || tabuleiro[posicaoJogada].equals("O")) {
+                System.out.println("Posição inválida");
+                return vez;
+            }
+            if (vez.equals(participante1)){
+                tabuleiro[posicaoJogada] = "X";
+                vez = participante2;
+            } else if (vez.equals(participante2)) {
+                tabuleiro[posicaoJogada] = "O";
+                vez = participante1;
+            }
+            Rodadas.rodadaAtual++;//incrementa mais um para atualizar as rodadas so se for uma posiçcao valida
+            return vez;
+        }catch (Exception E){
+            System.out.println("Posição Invalida!! Escolha um número disponível no tabuleiro, valores entre 1 a 9");
             return vez;
         }
-        if (vez.equals(participante1)){
-            tabuleiro[posicaoJogada] = "X";
-            vez = participante2;
-        } else if (vez.equals(participante2)) {
-            tabuleiro[posicaoJogada] = "O";
-            vez = participante1;
-        }
-        Rodadas.rodadaAtual++;//incrementa mais um para atualizar as rodadas so se for uma posiçcao valida
-        return vez;
     }
 }
